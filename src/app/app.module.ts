4662899fab5069de6af0a63a341cbec9858e227b
Alberto González Balderas <albertoglezb@hotmail.com>
2020-06-23 10:07:23 -0500

    The Assigment1 has finished.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WarningComponent } from './WarningComponent/Warning.component';
+import { SuccessComponent } from './success/success.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WarningComponent,
+    SuccessComponent
   ],
   imports: [
     BrowserModule,
