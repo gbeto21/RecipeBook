2f52ed2890da0b53abd2b1dfd9fb6040592e4c1f
Alberto González Balderas <albertoglezb@hotmail.com>
2020-06-25 08:30:22 -0500

    The funtionality to open the menus was created.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { RecipeItemComponent } from "./recipes/recipe-list/recipe-item/recipe-item.component";
 import { FormsModule } from '@angular/forms';
+import { DropdownDirective } from "./shared/dropdown.directive";
 
 @NgModule({
   declarations: [
@@ -21,7 +22,9 @@ import { FormsModule } from '@angular/forms';
     RecipeDetailComponent,
     RecipeItemComponent,
     ShoppingListComponent,
-    ShoppingEditComponent],
+    ShoppingEditComponent,
+    DropdownDirective
+  ],
     
   imports: [
     BrowserModule,
