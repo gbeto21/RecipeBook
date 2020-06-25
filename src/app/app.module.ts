e263db476d97259fd256c836965b5e8bd4b64849
Alberto González Balderas <albertoglezb@hotmail.com>
2020-06-25 15:58:32 -0500

    The Shopping-List-Service was created and implemented in the diferents components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-ed
 import { RecipeItemComponent } from "./recipes/recipe-list/recipe-item/recipe-item.component";
 import { FormsModule } from '@angular/forms';
 import { DropdownDirective } from "./shared/dropdown.directive";
+import { ShoppingListService } from "./shopping-list/shopping-list.service";
 
 @NgModule({
   declarations: [
@@ -31,7 +32,7 @@ import { DropdownDirective } from "./shared/dropdown.directive";
     AppRoutingModule,
     FormsModule
   ],
-  providers: [],
+  providers: [ShoppingListService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,18 @@
-export class ShoopingListService{
+import { Ingredient } from '../shared/ingredient.model';
+
+export class ShoppingListService{
     
+    private ingredients: Ingredient[] = [
+        new Ingredient('Apples', 5),
+        new Ingredient('Pies',10)
+    ]
+
+    getIngreients(){
+        return this.ingredients.slice();
+    }
+
+    addIngredient(ingredient: Ingredient){
+        this.ingredients.push(ingredient);
+    }
+
 }
