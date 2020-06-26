590bccbba2b766e077b371c7e4caf1912f22871c
Alberto González Balderas <albertoglezb@hotmail.com>
2020-06-25 19:11:44 -0500

    The ingredients were added to the recipe model.

--- src/app/recipes/recipe-list/recipe.service.ts
+++ src/app/recipes/recipe-list/recipe.service.ts
@@ -1,13 +1,27 @@
 import { Recipe } from './recipe.model';
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 export class RecipeService{
     
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('A test recipe', 'This is a test', 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.seriouseats.com%2Fimages%2F2016%2F07%2F20160711-eggplant-recipes-roundup-06-1500x1125.jpg&f=1&nofb=1'),
-        new Recipe('A diferent recipe', 'Diferent recipe', 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.seriouseats.com%2Fimages%2F2016%2F07%2F20160711-eggplant-recipes-roundup-06-1500x1125.jpg&f=1&nofb=1')
+        new Recipe(
+            'A test recipe', 
+            'This is a test', 
+            'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.seriouseats.com%2Fimages%2F2016%2F07%2F20160711-eggplant-recipes-roundup-06-1500x1125.jpg&f=1&nofb=1',
+            [
+                new Ingredient('Meat',1),
+                new Ingredient('French Fries',20),
+            ]),
+            new Recipe('A diferent recipe', 
+            'Diferent recipe', 
+            'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.seriouseats.com%2Fimages%2F2016%2F07%2F20160711-eggplant-recipes-roundup-06-1500x1125.jpg&f=1&nofb=1',
+            [
+                new Ingredient('Buns',2),
+                new Ingredient('Meat',1),
+            ])
     ]
       
     getRecipes(){
