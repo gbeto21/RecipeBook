34c71200fece63d44ba70e33efd50fa9f28ca104
Alberto González Balderas <albertoglezb@hotmail.com>
2020-06-27 16:19:14 -0500

    The bug with the ingredients was solved.

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -100,7 +100,9 @@ export class RecipeEditComponent implements OnInit {
     this.router.navigate(['../'], {relativeTo: this.route})
   }
 
-  onDeleteIngrediant(index:number){
+  onDeleteIngredient(index:number){
     (<FormArray>this.recipeForm.get('ingredients')).removeAt(index);
   }
 }
+//To delete all the ingredients.
+//(<FormArray>this.recipeForm.get('ingredients')).clear();
