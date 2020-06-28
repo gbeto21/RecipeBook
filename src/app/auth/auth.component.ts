2fad48838915c9ab05affb470254698fcd5d451a
Alberto González Balderas <albertoglezb@hotmail.com>
2020-06-28 11:52:51 -0500

    The header was modified validating if the user is log in.

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AuthService, AuthResponseData } from './auth.service';
 import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-auth',
@@ -13,7 +14,7 @@ export class AuthComponent{
     isLoading = false;
     error: string = null;
 
-    constructor(private authService: AuthService){}
+    constructor(private authService: AuthService, private router: Router){}
 
     onSwitchMode(){
         this.isLoginMode = !this.isLoginMode;
@@ -41,6 +42,7 @@ export class AuthComponent{
         authObs.subscribe(resData=>{
             this.isLoading = false;
             console.log(resData);
+            this.router.navigate(['/recipes'])
         },
         errorMessage=>{
             this.isLoading = false;
--- src/app/header/header/header.component.ts
+++ src/app/header/header/header.component.ts
@@ -1,22 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { DataStorageService } from 'src/app/shared/data-storage-service';
+import { AuthService } from 'src/app/auth/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
 
   collapsed = true;
+  private userSub: Subscription;
+  isAuthenticated = false;
 
   constructor(
-    private dataStoraeService: DataStorageService
+    private dataStoraeService: DataStorageService,
+    private authService: AuthService
   ) { }
 
   ngOnInit(): void {
+    this.userSub = this.authService.user.subscribe(user=>{
+      this.isAuthenticated = !!user;
+    })
   }
   
+  ngOnDestroy(){
+    this.userSub.unsubscribe();
+  }
+
   onSaveData(){
     this.dataStoraeService.storeRecipes();
   }
