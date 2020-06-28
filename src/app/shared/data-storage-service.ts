46bc5fdac51e040aeccdd0fbc685e5859c2e1bdf
Alberto González Balderas <albertoglezb@hotmail.com>
2020-06-28 12:58:26 -0500

    An interceptor was created.

--- src/app/shared/data-storage-service.ts
+++ src/app/shared/data-storage-service.ts
@@ -16,33 +16,39 @@ export class DataStorageService{
     storeRecipes(){
         const recipes = this.recipeService.getRecipes();
         this.http
-            .put('https://recipe-book-c2f8d.firebaseio.com/recipes.json',recipes)
-            .subscribe(response=>{
-
-            })
+        .put(
+            'https://recipe-book-c2f8d.firebaseio.com/recipes.json',
+            recipes
+        )
+        .subscribe(response => {
+            console.log(response);
+        });
     }
 
     fetchRecipes(){
         return this.authService.user.pipe(
-            take(1), 
-            exhaustMap(user=>{
-                return this.http
-                    .get<Recipe[]>('https://recipe-book-c2f8d.firebaseio.com/recipes.json',
-                    {
-                        params: new HttpParams().set('auth', user.Token)
-                    }
-                    )
+            take(1),
+            exhaustMap(user => {
+              return this.http.get<Recipe[]>(
+                'https://recipe-book-c2f8d.firebaseio.com/recipes.json',
+                {
+                  params: new HttpParams().set('auth', user.Token)
+                }
+              );
             }),
-            map(recipes=>{
-                return recipes.map(recipe=>{
-                    return {
-                        ...recipe, 
-                        ingredients: recipe.ingredients ? recipe.ingredients : []
-                    }
+            map(recipes => {
+              return recipes.map(recipe => {
+                return {
+                  ...recipe,
+                  ingredients: recipe.ingredients ? recipe.ingredients : []
+                };
+              });
+            }),
+            tap(recipes => {
+              this.recipeService.setRecipes(recipes);
             })
-        }), tap(recipes=>{
-            this.recipeService.setRecipes(recipes);
-        }))
+          );
+
     }
 
 }
