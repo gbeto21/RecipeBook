5ee01b2f409fa8762ff2e2842556c2049d2b2c79
Alberto González Balderas <albertoglezb@hotmail.com>
2020-06-28 10:56:49 -0500

    The login funtionality was implemented.

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { AuthService } from './auth.service';
+import { AuthService, AuthResponseData } from './auth.service';
+import { Observable } from 'rxjs';
 
 @Component({
     selector: 'app-auth',
@@ -26,24 +27,26 @@ export class AuthComponent{
 
         const email = form.value.email;
         const password = form.value.password;
+        let authObs: Observable<AuthResponseData>;
 
         this.isLoading=true;
 
         if(this.isLoginMode){
-
+            authObs = this.authService.login(email,password);
         }
         else{
-            this.authService.signup(email, password)
-            .subscribe(resData=>{
-                this.isLoading = false;
-                console.log(resData);
-            },
-            errorMessage=>{
-                this.isLoading = false;
-                this.error = errorMessage;
-                console.log(errorMessage);
-            });
+            authObs = this.authService.signup(email, password);
         }
+
+        authObs.subscribe(resData=>{
+            this.isLoading = false;
+            console.log(resData);
+        },
+        errorMessage=>{
+            this.isLoading = false;
+            this.error = errorMessage;
+            console.log(errorMessage);
+        });
 
         form.reset();        
     }
