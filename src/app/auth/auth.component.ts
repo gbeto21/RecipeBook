5439e1b748ab3e431ff87b85266d07079b93ad01
Alberto González Balderas <albertoglezb@hotmail.com>
2020-06-28 10:28:01 -0500

    An spining component was created and implemented on the auth component.

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -9,6 +9,8 @@ import { AuthService } from './auth.service';
 export class AuthComponent{
 
     isLoginMode = true;
+    isLoading = false;
+    error: string = null;
 
     constructor(private authService: AuthService){}
 
@@ -25,16 +27,21 @@ export class AuthComponent{
         const email = form.value.email;
         const password = form.value.password;
 
+        this.isLoading=true;
+
         if(this.isLoginMode){
 
         }
         else{
             this.authService.signup(email, password)
             .subscribe(resData=>{
+                this.isLoading = false;
                 console.log(resData);
             },
             error=>{
+                this.isLoading = false;
                 console.log(error);
+                this.error = 'An error occurred!'
             });
         }
 
