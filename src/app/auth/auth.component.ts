796536dc9e126c112369bfac75265f5583c703a9
Alberto González Balderas <albertoglezb@hotmail.com>
2020-06-28 16:37:35 -0500

    The funtionality of create a componen from code has finished..

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,7 +1,7 @@
-import { Component, ComponentFactoryResolver, ViewChild } from '@angular/core';
+import { Component, ComponentFactoryResolver, ViewChild, OnDestroy } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AuthService, AuthResponseData } from './auth.service';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { Router } from '@angular/router';
 import { AlertComponent } from "../shared/alert/alert.component";
 import { PlaceholderDirective } from '../shared/placeholder/placeholder.directive';
@@ -10,12 +10,14 @@ import { PlaceholderDirective } from '../shared/placeholder/placeholder.directiv
     selector: 'app-auth',
     templateUrl: './auth.component.html'
 })
-export class AuthComponent{
+export class AuthComponent implements OnDestroy{
 
     isLoginMode = true;
     isLoading = false;
     error: string = null;
     @ViewChild(PlaceholderDirective, {static: false}) alertHost: PlaceholderDirective;
+
+    private closeSub: Subscription;
 
     constructor(
         private authService: AuthService, 
@@ -24,6 +26,12 @@ export class AuthComponent{
 
     onSwitchMode(){
         this.isLoginMode = !this.isLoginMode;
+    }
+
+    ngOnDestroy(){
+        if(this.closeSub){
+            this.closeSub.unsubscribe();
+        }
     }
 
     onSubmit(form: NgForm){
@@ -71,7 +79,12 @@ export class AuthComponent{
         const hostViewContainerRef = this.alertHost.viewContainerRef;
         hostViewContainerRef.clear();
 
-        hostViewContainerRef.createComponent(alectCmpFactory);
+        const componentRef = hostViewContainerRef.createComponent(alectCmpFactory);
+        componentRef.instance.message = message;
+        this.closeSub = componentRef.instance.close.subscribe(()=>{
+            this.closeSub.unsubscribe();
+            hostViewContainerRef.clear();
+        });
 
     }
 
