a3e497f604e2fa81ed159ca2527dec6ec214d9ed
Alberto González Balderas <albertoglezb@hotmail.com>
2020-06-27 20:37:05 -0500

    The funtionality to don't fetch data if the server has recipes was implemented.

--- src/app/recipes/recipes-resolver.service.ts
+++ src/app/recipes/recipes-resolver.service.ts
@@ -2,15 +2,25 @@ import { Injectable } from '@angular/core';
 import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Recipe } from './recipe-list/recipe.model';
 import { DataStorageService } from '../shared/data-storage-service';
+import { RecipeService } from './recipe-list/recipe.service';
 
 
 @Injectable({providedIn: 'root'})
 export class RecipesResolveService implements Resolve<Recipe[]>{
 
-    constructor(private dataStorageService: DataStorageService){}
+    constructor(
+        private dataStorageService: DataStorageService, 
+        private recipesService: RecipeService){}
 
     resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot){
-        return this.dataStorageService.fetchRecipes();
+        const recipes = this.recipesService.getRecipes();
+
+        if(recipes.length === 0){
+            return this.dataStorageService.fetchRecipes();
+        }
+        else{
+            return recipes;
+        }
     }
 
 }
