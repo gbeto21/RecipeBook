7afe108928b77c8821aa9a7e11e0c4768fec97c8
Alberto González Balderas <albertoglezb@hotmail.com>
2020-06-28 13:48:48 -0500

    The funtionality to auto log out was implemented.

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -19,6 +19,7 @@ export interface AuthResponseData{
 export class AuthService{
 
     user = new BehaviorSubject<User>(null);
+    private tokenExpirationTimer: any;
 
     constructor(
         private http: HttpClient,
@@ -85,10 +86,12 @@ export class AuthService{
             userData.id, 
             userData._token, 
             new Date(userData._tokenExpirationDate)
-        );
-
-        if(loadedUser.Token){
+            );
+            
+            if(loadedUser.Token){
             this.user.next(loadedUser);
+            const expirationDuration = new Date(userData._tokenExpirationDate).getTime() - new Date().getTime() 
+            this.autoLogout(expirationDuration);
         }
 
     }
@@ -96,6 +99,17 @@ export class AuthService{
     logout(){
         this.user.next(null);
         this.router.navigate(['/auth']);
+        localStorage.removeItem('userData');
+        if(this.tokenExpirationTimer){
+            clearTimeout(this.tokenExpirationTimer);
+        }
+        this.tokenExpirationTimer = null;
+    }
+
+    autoLogout(expirationDuration: number){
+        this.tokenExpirationTimer = setTimeout(()=>{
+            this.logout();
+        },expirationDuration);
     }
 
     private handleAuthentication(email: string, userId: string, token: string, expiresIn: number){
@@ -107,6 +121,7 @@ export class AuthService{
             token, 
             expirationDate);
             this.user.next(user);
+            this.autoLogout(expiresIn * 1000);
             localStorage.setItem('userData', JSON.stringify(user))
     }
 
