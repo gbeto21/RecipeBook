2a4bb937c15c2a73ad4baddc95b6eb1df1ea1e1b
Alberto González Balderas <albertoglezb@hotmail.com>
2020-06-29 06:49:29 -0500

    The core module was implemented.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header/header.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { ShoppingListService } from "./shopping-list/shopping-list.service";
-import { RecipeService } from './recipes/recipe-list/recipe.service';
 import { AuthComponent } from './auth/auth.component';
-import { AuthInterceptorService } from './auth/auth-interceptor.service';
 import { RecipesMoule } from './recipes/recipes.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core.moule';
 
 @NgModule({
   declarations: [
@@ -27,16 +25,9 @@ import { SharedModule } from './shared/shared.module';
     AppRoutingModule,
     RecipesMoule,
     ShoppingListModule,
-    SharedModule
+    SharedModule,
+    CoreModule
   ],
-  providers: [
-    ShoppingListService, 
-    RecipeService, 
-    {
-      provide: HTTP_INTERCEPTORS, 
-      useClass: AuthInterceptorService, 
-      multi: true
-    }],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
