1c37113cb7fe388ade5a5145920fd15ad4906742
Alberto González Balderas <albertoglezb@hotmail.com>
2020-06-29 07:39:17 -0500

    Preloading was implemented.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,8 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header/header.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { RecipesMoule } from './recipes/recipes.module';
-import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.moule';
-import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
