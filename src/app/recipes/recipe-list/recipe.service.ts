663fcbc33ffe8d68bcda63b87a9283a953b9a76f
Alberto González Balderas <albertoglezb@hotmail.com>
2020-06-30 08:06:21 -0500

    The shopping-list actions was added with update ingredient and delete ingredient.

--- src/app/recipes/recipe-list/recipe.service.ts
+++ src/app/recipes/recipe-list/recipe.service.ts
@@ -11,24 +11,6 @@ export class RecipeService{
 
     recipiesChanged = new Subject<Recipe[]>();
     
-    // private recipes: Recipe[] = [
-    //     new Recipe(
-    //         'A test recipe', 
-    //         'This is a test', 
-    //         'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.seriouseats.com%2Fimages%2F2016%2F07%2F20160711-eggplant-recipes-roundup-06-1500x1125.jpg&f=1&nofb=1',
-    //         [
-    //             new Ingredient('Meat',1),
-    //             new Ingredient('French Fries',20),
-    //         ]),
-    //         new Recipe('A diferent recipe', 
-    //         'Diferent recipe', 
-    //         'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.seriouseats.com%2Fimages%2F2016%2F07%2F20160711-eggplant-recipes-roundup-06-1500x1125.jpg&f=1&nofb=1',
-    //         [
-    //             new Ingredient('Buns',2),
-    //             new Ingredient('Meat',1),
-    //         ])
-    // ]
-    
     private recipes: Recipe[] = [];
 
     constructor(
